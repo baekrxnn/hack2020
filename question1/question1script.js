1ec8bda889a28dc6ca5794590a6ef163a63bbfab
Glitch (hello-webpage) <none>
2020-02-07 19:31:46 +0000

    🍞⛵️ Checkpoint ./question1/question1script.js:7554858/14:11154864/504 ./question1/style.css:3247354/203 ./index.html:3247354/37 ./question1/index.html:7554858/149

--- question1/question1script.js
+++ question1/question1script.js
@@ -9,6 +9,11 @@ $("#answer1").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 25){
-    $(".answerSpace").text("thats correct! About 25 percent of jobs in tech are filled by women")
-  } else if(answer < 25)
+    $(".answerSpace").text("thats correct! Only About 25 percent of jobs in tech are filled by women, while they make up 47 percent of the workforce")
+    $(0).text
+  } else if(answer < 25) {
+    $(".answerSpace").text("Almost, but you're a little too low...")
+  } else {
+    $(".answerSpace").text("Not quite that high...")
+  }
 });
