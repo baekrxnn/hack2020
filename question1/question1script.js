ae53b8cb241ba43733842ac0010dd4bd4a012af8
Glitch (hackathon2020-t14) <none>
2020-02-07 22:19:45 +0000

    💈🌴 Checkpoint ./question1/question1script.js:789040/4554:11154864/2 ./question1/q1.html:7554858/105:11154864/98 ./question1/style.css:7554858/48:11154864/65 ./index.html:3247354/216 ./title/style.css:3247354/374 ./question1/q2.html:7554858/208:11154864/3293 ./question1/q3.html:11154864/3300 ./question1/q4.html:11154864/3286 ./question1/q5.html:11154864/3268 ./question1/q6.html:11154864/3286 ./question1/q7.html:11154864/3276 ./question1/q8.html:11154864/3288

--- question1/question1script.js
+++ question1/question1script.js
@@ -10,9 +10,9 @@ $("#answer1").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 25){
-    $(".answerSpace").text("thats correct! Only About 25 percent of jobs in tech are filled by women, while they make up 47 percent of the workforce")
+    $(".answerSpace").text("thats correct! Only About 25 percent of jobs in tech are filled by women, while they make up 47 percent of the workforce");
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 25) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -26,10 +26,108 @@ $("#answer2").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 3){
-    $(".answerSpace").text("thats correct! As of 2016, only About 3 percent of tech jobs at facebook are filled by people who identify as Hispanic")
+    $(".answerSpace").text("thats correct! As of 2016, only About 3 percent of tech jobs at facebook are filled by people who identify as Hispanic");
     $('.dropBtn').show();
     $(".moveON").show();
-  } else if(answer < 25) {
+  } else if(answer < 3) {
+    $(".answerSpace").text("Almost, but you're a little too low...")
+    window.counter =+ 1
+  } else {
+    $(".answerSpace").text("Not quite that high...")
+    window.counter =+ 1 
+  }
+});
+
+$("#answer3").click(function() {
+  let answer = $("#slider").slider("value");
+  console.log(answer);
+  if(answer === 18){
+    $(".answerSpace").text("thats correct! As of 2016, only About 18 percent of tech jobs at Google (worldwide!) are filled by people who identify as women");
+    $('.dropBtn').show();
+    $(".moveON").show();
+  } else if(answer < 18) {
+    $(".answerSpace").text("Almost, but you're a little too low...")
+    window.counter =+ 1
+  } else {
+    $(".answerSpace").text("Not quite that high...")
+    window.counter =+ 1 
+  }
+});
+
+$("#answer4").click(function() {
+  let answer = $("#slider").slider("value");
+  console.log(answer);
+  if(answer === 41){
+    $(".answerSpace").text("thats correct! As of 2016, Companies are only interviewing men 41% of the time for tech jobs");
+    $('.dropBtn').show();
+    $(".moveON").show();
+  } else if(answer < 41) {
+    $(".answerSpace").text("Almost, but you're a little too low...")
+    window.counter =+ 1
+  } else {
+    $(".answerSpace").text("Not quite that high...")
+    window.counter =+ 1 
+  }
+});
+
+$("#answer5").click(function() {
+  let answer = $("#slider").slider("value");
+  console.log(answer);
+  if(answer === 93){
+    $(".answerSpace").text("thats correct! Although better than many other industires, women make $0.93 per dollar a man in similar roles make");
+    $('.dropBtn').show();
+    $(".moveON").show();
+  } else if(answer < 93) {
+    $(".answerSpace").text("Almost, but you're a little too low...")
+    window.counter =+ 1
+  } else {
+    $(".answerSpace").text("Not quite that high...")
+    window.counter =+ 1 
+  }
+});
+
+$("#answer6").click(function() {
+  let answer = $("#slider").slider("value");
+  console.log(answer);
+  if(answer === 6){
+    $(".answerSpace").text("thats correct! Women are offered 6% less in salary compared to a man applying for the same position");
+    $('.dropBtn').show();
+    $(".moveON").show();
+  } else if(answer < 6) {
+    $(".answerSpace").text("Almost, but you're a little too low...")
+    window.counter =+ 1
+  } else {
+    $(".answerSpace").text("Not quite that high...")
+    window.counter =+ 1 
+  }
+});
+
+$("#answer7").click(function() {
+  let answer = $("#slider").slider("value");
+  console.log(answer);
+  if(answer === 77){
+    $(".answerSpace").text("thats correct! San Francisco's rent is 277% over the national average");
+    // info: san francisco's average rent (monthly) is $4500; national average is $1625 per month.
+    $('.dropBtn').show();
+    $(".moveON").show();
+  } else if(answer < 77) {
+    $(".answerSpace").text("Almost, but you're a little too low...")
+    window.counter =+ 1
+  } else {
+    $(".answerSpace").text("Not quite that high...")
+    window.counter =+ 1 
+  }
+});
+
+("#answer7").click(function() {
+  let answer = $("#slider").slider("value");
+  console.log(answer);
+  if(answer === 77){
+    $(".answerSpace").text("thats correct! San Francisco's rent is 277% over the national average");
+    // info: san francisco's average rent (monthly) is $4500; national average is $1625 per month.
+    $('.dropBtn').show();
+    $(".moveON").show();
+  } else if(answer < 77) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
   } else {
