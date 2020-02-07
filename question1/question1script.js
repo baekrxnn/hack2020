85e140875e47c0656cefb0809b2a2043bb483c64
Glitch (hackathon2020-t14) <none>
2020-02-07 22:45:36 +0000

    Exporting to baekrxnn/hackathon2020 ./question1/style.css:3247354/41 ./question1/question1script.js:789040/214:11154864/35 ./question1/q2.html:789040/206 ./question1/q1.html:3247354/70:11154864/80 ./end/results.html:7554858/196

--- question1/question1script.js
+++ question1/question1script.js
@@ -9,8 +9,9 @@ $(function() {
 $("#answer1").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
+  $(".percentNum").text(answer);
   if(answer === 25){
-    $(".answerSpace").text("thats correct! Only About 25 percent of jobs in tech are filled by women, while they make up 47 percent of the workforce");
+    $(".answerSpace").text("thats correct!");
     $('.dropBtn').show();
     $(".moveOn").show();
   } else if(answer < 25) {
@@ -26,9 +27,9 @@ $("#answer2").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 3){
-    $(".answerSpace").text("thats correct! As of 2016, only About 3 percent of tech jobs at facebook are filled by people who identify as Hispanic");
+    $(".answerSpace").text("thats correct!");
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 3) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -42,9 +43,9 @@ $("#answer3").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 18){
-    $(".answerSpace").text("thats correct! As of 2016, only About 18 percent of tech jobs at Google (worldwide!) are filled by people who identify as women");
+    $(".answerSpace").text("thats correct!");
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 18) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -58,9 +59,9 @@ $("#answer4").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 41){
-    $(".answerSpace").text("thats correct! As of 2016, Companies are only interviewing men 41% of the time for tech jobs");
+    $(".answerSpace").text("thats correct!");
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 41) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -74,9 +75,9 @@ $("#answer5").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 93){
-    $(".answerSpace").text("thats correct! Although better than many other industires, women make $0.93 per dollar a man in similar roles make");
+    $(".answerSpace").text("thats correct!");
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 93) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -90,9 +91,9 @@ $("#answer6").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 6){
-    $(".answerSpace").text("thats correct! Women are offered 6% less in salary compared to a man applying for the same position");
+    $(".answerSpace").text("thats correct!");
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 6) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -106,10 +107,10 @@ $("#answer7").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 77){
-    $(".answerSpace").text("thats correct! San Francisco's rent is 277% over the national average");
+    $(".answerSpace").text("thats correct!");
     // info: san francisco's average rent (monthly) is $4500; national average is $1625 per month.
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 77) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -123,10 +124,10 @@ $("#answer8").click(function() {
   let answer = $("#slider").slider("value");
   console.log(answer);
   if(answer === 50){
-    $(".answerSpace").text("thats correct! About 50% of monthly earnings go towards paying for rent for an engineer in San Francisco");
+    $(".answerSpace").text("thats correct!");
     // info: san francisco's average rent (monthly) is $4500; national average is $1625 per month.
     $('.dropBtn').show();
-    $(".moveON").show();
+    $(".moveOn").show();
   } else if(answer < 50) {
     $(".answerSpace").text("Almost, but you're a little too low...")
     window.counter =+ 1
@@ -139,3 +140,5 @@ $("#answer8").click(function() {
 $('.dropBtn').click(function() {
   $('.dropInfo').show();
 });
+
+console.log(window.counter);
