fd9fb6b4ef5164228bcae4a9255f5ba257e68660
Glitch (hello-webpage) <none>
2020-02-07 18:20:46 +0000

    🌵🎉 Checkpoint ./index.html:11154864/311 ./script.js:789040/17:11154864/337

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+/* global $*/
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+$("button").click(function() {
+  $("button").hide;
+});
